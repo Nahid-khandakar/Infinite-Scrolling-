4e7890cd7798a17353ccc60d7a43d6bbd36475af
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-06-10 04:17:52 +0600

    home page update

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
         <div>
             <h1 className='text-center py-5 text-2xl font-bold'>Random User </h1>
 
-            <div>
+            <div className='mt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-y-9'>
 
                 {
                     users?.map((user, index) => <Usercard
