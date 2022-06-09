594856b4e1434fc57160a751df3e4f2d7255fd5a
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-06-10 00:15:44 +0600

    login from add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './page/Authentication/Login';
+import Signup from './page/Authentication/Signup';
+import Home from './page/Home/Home';
+import Navbar from './page/Navbar/Navbar';
+import Error from './page/Error/Error'
 
 function App() {
   return (
     <div >
-      <h1>This is tailwind</h1>
+      <Navbar></Navbar>
+
+      <Routes>
+
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<Signup></Signup>}></Route>
+
+
+        <Route path='*' element={<Error></Error>}></Route>
+
+      </Routes>
     </div>
   );
 }
--- src/page/Navbar/Navbar.js
+++ src/page/Navbar/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            this is Navbar
+        <div className="navbar bg-primary">
+
+
+            <div className="navbar-start">
+                <Link to='/' className="btn btn-ghost normal-case text-2xl text-white font-bold">Infinite Scrolling</Link>
+            </div>
+
+
+            <div className="navbar-end mr-4">
+                <Link to="/login" className="text-md text-neutral font-bold uppercase">Log in</Link>
+            </div>
+
+
         </div>
     );
 };
