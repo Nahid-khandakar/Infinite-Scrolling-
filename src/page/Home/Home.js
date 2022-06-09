ff440a372cc4b0628b260938e5e8b75f10db0f02
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-06-10 05:47:40 +0600

    skeleton animation add

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -2,22 +2,31 @@ import React, { useEffect, useState } from 'react';
 import Usercard from '../Usercard/Usercard';
 
 
+
 const Home = () => {
 
     const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)
 
 
-    const url = "https://randomuser.me/api/?results=10"
+    const url = "https://randomuser.me/api/?results=1500"
+
     useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setUsers(data.results))
-    }, [])
+
+        setTimeout(async () => {
+            const res = await fetch(url)
+            const data = await res.json()
+            setUsers(data.results)
+        }, 1000);
+
+    }, [loading])
 
 
     return (
         <div>
             <h1 className='text-center py-5 text-2xl font-bold'>Random User </h1>
+
+
 
             <div className='mt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-y-9'>
 
@@ -25,11 +34,11 @@ const Home = () => {
                     users?.map((user, index) => <Usercard
                         key={index}
                         user={user}
+                        setLoading={setLoading}
                     ></Usercard>)
                 }
 
             </div>
-
 
         </div>
     );
--- src/page/Usercard/Usercard.js
+++ src/page/Usercard/Usercard.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
-const Usercard = ({ user }) => {
+const Usercard = ({ user, setLoading }) => {
 
     return (
 
-        <div className='px-5'>
+        <>
+            {
+                setLoading === true ?
 
-            <div className="max-w-2xl mx-auto">
-
-                <div className="max-w-sm bg-primary rounded-lg py-5 ">
-
-                    <div className="flex flex-col items-center pb-10">
-                        <img src={user.picture.medium} className="mb-3 w-24 h-24 rounded-full shadow-lg" alt="random user" />
-                        <h3 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{user?.name?.first + " " + user?.name?.last}</h3>
-                        <h3 className="text-md text-gray-900">{user?.location?.country}</h3>
-                        <h3 className="text-md text-gray-900">{user?.email}</h3>
-                        <h3 className="text-md text-gray-900">{user?.phone}</h3>
+                    <div className='px-5'>
+                        <div className="max-w-2xl mx-auto">
+                            <div className='max-w-sm bg-white shadow-lg rounded-lg py-5 '>
+                                <div className='text-center items-center pb-10'>
+                                    <Skeleton className='mb-3 w-24 h-24 rounded-full shadow-mg mx-auto' circle={true} height={96} width={96} />
+                                    <h3 className='px-10'><Skeleton /></h3>
+                                    <h3 className='text-center px-14'><Skeleton /></h3>
+                                    <h3 className='text-md px-8'><Skeleton /></h3>
+                                    <h3 className='text-md px-16'><Skeleton /></h3>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-
-        </div>
+                    :
+                    <div className='px-5'>
+                        <div className="max-w-2xl mx-auto">
+                            <div className="max-w-sm bg-primary rounded-lg py-5 ">
+                                <div className="text-center items-center pb-10">
+                                    <img src={user.picture.medium} className="mb-3 w-24 h-24 rounded-full shadow-lg mx-auto" alt="random user" />
+                                    <h3 className="mb-1 text-xl font-medium text-gray-900">{user?.name?.first + " " + user?.name?.last}</h3>
+                                    <h3 className="text-md text-gray-900">{user?.location?.country}</h3>
+                                    <h3 className="text-md text-gray-900">{user?.email}</h3>
+                                    <h3 className="text-md text-gray-900">{user?.phone}</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            }
+        </>
     );
 };
 
