efd98e549baad12b2d1db4bc8927efbfa8f5b2bd
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-06-10 04:12:44 +0600

    user card ready

--- src/page/Usercard/Usercard.js
+++ src/page/Usercard/Usercard.js
@@ -3,10 +3,23 @@ import React from 'react';
 const Usercard = ({ user }) => {
 
     return (
-        <div>
-            <h1>{user.gender}</h1>
-            <h1>{user.name.first}</h1>
-            <img src={user.picture.medium} alt="" />
+
+        <div className='px-5'>
+
+            <div className="max-w-2xl mx-auto">
+
+                <div className="max-w-sm bg-primary rounded-lg py-5 ">
+
+                    <div className="flex flex-col items-center pb-10">
+                        <img src={user.picture.medium} className="mb-3 w-24 h-24 rounded-full shadow-lg" alt="random user" />
+                        <h3 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{user?.name?.first + " " + user?.name?.last}</h3>
+                        <h3 className="text-md text-gray-900">{user?.location?.country}</h3>
+                        <h3 className="text-md text-gray-900">{user?.email}</h3>
+                        <h3 className="text-md text-gray-900">{user?.phone}</h3>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
