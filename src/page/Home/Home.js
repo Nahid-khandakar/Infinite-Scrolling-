023edaed01d015ce3754ae70da61c7d656387a95
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-06-10 14:39:05 +0600

    load 1000 user

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     const [users, setUsers] = useState([])
     const [loading, setLoading] = useState(false)
 
-    const url = "https://randomuser.me/api/?results=2000"
+    const url = "https://randomuser.me/api/?results=1000"
 
     useEffect(() => {
         setLoading(true)
